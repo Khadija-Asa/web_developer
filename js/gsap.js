b52b15269a71351d1746dd423388a617ae919e06
Khadidja <khadija.aitsiali@gmail.com>
2024-09-13 15:36:36 +0200

    add animation to grid gallery

--- js/gsap.js
+++ js/gsap.js
@@ -1,21 +1,21 @@
-var gsap = gsap.timeline({ defaults: { duration: 2, ease: "power3.out" } });
-gsap.from(".hero_year", { 
-  opacity: 0,
-  x: 300 
-}, 1.5);
-gsap.from(".hero_img", {
-  opacity: 0,
-  x: 500
-}, 1);
-gsap.from(".grid_text", {
-  opacity: 0,
-  y: 400
-}, 2);
-gsap.from(".grid_name", {
-  opacity: 0,
-  y: 400
-}, 2.1);
-gsap.from(".grid_asterisque", {
-  opacity: 0,
-  y: 400
-}, 2.2);
+// var gsap = gsap.timeline({ defaults: { duration: 2, ease: "power3.out" } });
+// gsap.from(".hero_year", { 
+//   opacity: 0,
+//   x: 300 
+// }, 1.5);
+// gsap.from(".hero_img", {
+//   opacity: 0,
+//   x: 500
+// }, 1);
+// gsap.from(".grid_text", {
+//   opacity: 0,
+//   y: 400
+// }, 2);
+// gsap.from(".grid_name", {
+//   opacity: 0,
+//   y: 400
+// }, 2.1);
+// gsap.from(".grid_asterisque", {
+//   opacity: 0,
+//   y: 400
+// }, 2.2);
