f1be29bed054950cfa09240bec4712fd53a2a29c
Khadidja <khadija.aitsiali@gmail.com>
2024-09-20 23:37:07 +0200

    add animation on a asterisque img

--- js/gsap.js
+++ js/gsap.js
@@ -19,3 +19,18 @@
 //   opacity: 0,
 //   y: 400
 // }, 2.2);
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".asterisque", {
+  scrollTrigger: {
+    trigger: ".asterisque",
+    scrub: true,
+    start: "left center",
+    end: "+=5000",
+    pin: true,
+  },
+  x: 400,
+  rotation: 720,
+  ease: "linear"
+});
