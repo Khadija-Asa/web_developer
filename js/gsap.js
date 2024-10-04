896b282d714c9922625cd1c565518669dd2b54ad
Khadidja <khadija.aitsiali@gmail.com>
2024-10-04 23:46:23 +0200

    done work page + add animations

--- js/gsap.js
+++ js/gsap.js
@@ -1,14 +1,14 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".asterisque", {
-  scrollTrigger: {
-    trigger: ".asterisque",
-    scrub: true,
-    start: "left center",
-    end: "+=5000",
-    pin: true,
-  },
-  x: 400,
-  rotation: 720,
-  ease: "ease"
-});
+// gsap.to(".asterisque", {
+//   scrollTrigger: {
+//     trigger: ".asterisque",
+//     scrub: true,
+//     start: "left center",
+//     end: "+=5000",
+//     pin: true,
+//   },
+//   x: 400,
+//   rotation: 720,
+//   ease: "ease"
+// });
