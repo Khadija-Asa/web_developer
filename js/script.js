dea4b3095c9d3d92b1db522a4887d51d79160335
Khadidja <khadija.aitsiali@gmail.com>
2024-10-05 16:43:31 +0200

    add animation + loading

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Overlay
+gsap.to(".first", {
+  duration: 1.5,
+  delay: 0.6,
+  left: "-100%",
+  ease: "expo.inOut"
+});
+
+gsap.to(".second", {
+  duration: 1.5,
+  delay: 0.8,
+  left: "-100%",
+  ease: "expo.inOut"
+});
