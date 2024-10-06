293ba557de9d67370f3d3223c4224871ba3b8b62
Khadidja <khadija.aitsiali@gmail.com>
2024-10-06 20:33:12 +0200

    add new feature to contact page + add loading + add new animations

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Overlay
 gsap.to(".first", {
-  duration: 1.5,
+  duration: 1,
   delay: 0.6,
   left: "-100%",
   ease: "expo.inOut"
 });
 
 gsap.to(".second", {
-  duration: 1.5,
+  duration: 1,
   delay: 0.8,
   left: "-100%",
   ease: "expo.inOut"
