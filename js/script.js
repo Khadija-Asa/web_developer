3d230647cd374baecef24772485160e2d7f25d46
Khadidja <khadija.aitsiali@gmail.com>
2024-10-07 19:34:31 +0200

    add text in loading part + add marquee back

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Overlay
 gsap.to(".first", {
   duration: 1,
-  delay: 0.6,
+  delay: 1,
   left: "-100%",
   ease: "expo.inOut"
 });
 
 gsap.to(".second", {
   duration: 1,
-  delay: 0.8,
+  delay: 1.2,
   left: "-100%",
   ease: "expo.inOut"
 });
