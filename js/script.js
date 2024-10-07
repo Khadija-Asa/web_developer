6e943f8eb569eb00537df681a1d28dc7b6fe37d0
Khadidja <khadija.aitsiali@gmail.com>
2024-10-07 23:45:58 +0200

    compressed images

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-// Overlay
-gsap.to(".first", {
-  duration: 1,
-  delay: 1,
-  left: "-100%",
-  ease: "expo.inOut"
-});
+// load fast
+function loadGSAP() {
+  var script = document.createElement('script');
+  script.src = "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js";
+  document.body.appendChild(script);
+}
 
-gsap.to(".second", {
-  duration: 1,
-  delay: 1.2,
-  left: "-100%",
-  ease: "expo.inOut"
-});
+// Overlay
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.to(".first", {
+    duration: 1,
+    delay: 1,
+    left: "-100%",
+    ease: "expo.inOut"
+  });
+  
+  gsap.to(".second", {
+    duration: 1,
+    delay: 1.2,
+    left: "-100%",
+    ease: "expo.inOut"
+  });
+ });
