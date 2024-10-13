f7196a9b23680a8efe697ad4b4b0052481089fd0
Khadidja <khadija.aitsiali@gmail.com>
2024-10-13 19:35:04 +0200

    add scroll to top + loader

--- js/script.js
+++ js/script.js
@@ -7,16 +7,46 @@ function loadGSAP() {
 
 // Overlay
 document.addEventListener("DOMContentLoaded", (event) => {
-  // gsap.to(".first", {
-  //   duration: 3,
-  //   delay: 0,
-  //   left: "-100%",
-  //   ease: "expo.inOut"
-  // });
-  // gsap.to(".second", {
-  //   duration: 3,
-  //   delay: 0.2,
-  //   left: "-100%",
-  //   ease: "expo.inOut"
-  // });
+  gsap.to(".first", {
+    duration: 2.5,
+    delay: 0,
+    left: "-100%",
+    ease: "expo.inOut"
+  });
+  gsap.to(".second", {
+    duration: 2.5,
+    delay: 0.2,
+    left: "-100%",
+    ease: "expo.inOut"
+  });
+  gsap.to(".third", {
+    duration: 2.5,
+    delay: 0,
+    left: "-100%",
+    ease: "expo.inOut"
+  });
 });
+
+// Scroll to top
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
